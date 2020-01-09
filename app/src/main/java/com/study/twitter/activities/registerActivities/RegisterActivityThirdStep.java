b1f2144cd3edaba8f86d21a4eb379927b1027eff
gunjan <gunjan.shakya3@gmail.com>
2020-01-09 20:48:05 +0545

    Web view added

--- app/src/main/java/com/study/twitter/activities/registerActivities/RegisterActivityThirdStep.java
+++ app/src/main/java/com/study/twitter/activities/registerActivities/RegisterActivityThirdStep.java
@@ -3,14 +3,38 @@ package com.study.twitter.activities.registerActivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.study.twitter.R;
 
 public class RegisterActivityThirdStep extends AppCompatActivity {
+    private TextView textView3;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_third_step);
+        binding();
+        String text = "By signing up, you agree to the Terms of service and Privacy Policy, including Cookie Use. Others will be able to find you by email or phone number when provided Privacy Options";
+        SpannableString ss = new SpannableString(text);
+
+
+//
+//        ss.setSpan(clickableSpanTerms, 32, 48, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        ss.setSpan(clickableSpanPP, 53, 67, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        ss.setSpan(clickableSpanCookie, 79, 89, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        ss.setSpan(clickableSpanPO, 162, 177, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView3.setText(ss);
+        textView3.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private void binding(){
+        textView3 = findViewById(R.id.textView3);
+        btnSignUp = findViewById(R.id.btnSignUp);
     }
 }
---
+++ app/src/main/java/com/study/twitter/activities/webviewActivities/WebViewPP.java
@@ -0,0 +1,24 @@
+package com.study.twitter.activities.webviewActivities;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.study.twitter.R;
+
+public class WebViewPP extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view_help);
+        webView = findViewById(R.id.webView);
+        webView.loadUrl("https://help.twitter.com/en/managing-your-account/new-account-settings");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(true);
+    }
+}
