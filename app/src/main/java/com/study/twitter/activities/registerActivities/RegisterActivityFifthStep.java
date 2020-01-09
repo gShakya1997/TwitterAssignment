8775878b00fb3a057aa0381d532f82802ca1850b
gunjan <gunjan.shakya3@gmail.com>
2020-01-09 21:52:00 +0545

    Register fifth step design completed

---
+++ app/src/main/java/com/study/twitter/activities/registerActivities/RegisterActivityFifthStep.java
@@ -0,0 +1,16 @@
+package com.study.twitter.activities.registerActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.study.twitter.R;
+
+public class RegisterActivityFifthStep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_fifth_step);
+    }
+}
