250d9896f0d7360928a3c4861ef412e282c6be99
gunjan <gunjan.shakya3@gmail.com>
2020-01-10 12:24:02 +0545

    Issue in img upload

--- app/src/main/java/com/study/twitter/activities/registerActivities/RegisterActivityFifthStep.java
+++ app/src/main/java/com/study/twitter/activities/registerActivities/RegisterActivityFifthStep.java
@@ -16,23 +16,32 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.study.twitter.R;
+import com.study.twitter.activities.LoginActivity;
 import com.study.twitter.api.UsersAPI;
+import com.study.twitter.model.DataTransfer;
+import com.study.twitter.model.Users;
 import com.study.twitter.serverResponse.ImageResponse;
+import com.study.twitter.serverResponse.RegisterResponse;
+import com.study.twitter.strictMode.StrictModeClass;
 import com.study.twitter.url.Url;
 
 import java.io.File;
+import java.io.IOException;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class RegisterActivityFifthStep extends AppCompatActivity {
     private ImageView imgBtnBackFifthStep;
     private ImageButton imgBtnUpload;
     private Button btnSignUpFinal;
-    private String imgPath;
+    String imgPath;
+    private String imgName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +54,8 @@ public class RegisterActivityFifthStep extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK){
-            if (data == null){
+        if (resultCode == RESULT_OK) {
+            if (data == null) {
                 Toast.makeText(this, "Please select and image", Toast.LENGTH_SHORT).show();
             }
         }
@@ -67,11 +76,22 @@ public class RegisterActivityFifthStep extends AppCompatActivity {
         return result;
     }
 
-    private void actionButtons(){
+    private void actionButtons() {
         imgBtnBackFifthStep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivityFifthStep.this,RegisterActivityFourthStep.class);
+                Intent intent = new Intent(RegisterActivityFifthStep.this, RegisterActivityFourthStep.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnSignUpFinal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveImgOnly();
+                register();
+                Intent intent = new Intent(RegisterActivityFifthStep.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -91,16 +111,51 @@ public class RegisterActivityFifthStep extends AppCompatActivity {
         startActivityForResult(intent, 0);
     }
 
-    private void saveImgOnly(){
+    private void saveImgOnly() {
         File file = new File(imgPath);
         RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
-        MultipartBody.Part body = MultipartBody.Part.createFormData("imgfile",file.getName(),requestBody);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("imagefile", file.getName(), requestBody);
         UsersAPI usersAPI = Url.getInstance().create(UsersAPI.class);
         Call<ImageResponse> responseCall = usersAPI.uploadImage(body);
-
+        StrictModeClass.StrictMode(); //from StrictModeClass
+        try {
+            Response<ImageResponse> imageResponseResponse = responseCall.execute();
+            imgName = imageResponseResponse.body().getFilename();
+            Toast.makeText(this, "Image inserted " + imgName, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error " + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
-    private void binding(){
+    private void register(){
+        String username = DataTransfer.name;
+        String password = DataTransfer.password;
+        String email = DataTransfer.email;
+        String phone = DataTransfer.phone;
+
+        Users users = new Users(username, password, email, phone, imgName);
+        UsersAPI usersAPI = Url.getInstance().create(UsersAPI.class);
+        Call<RegisterResponse> registerResponseCall = usersAPI.registerUser(users);
+
+        registerResponseCall.enqueue(new Callback<RegisterResponse>() {
+            @Override
+            public void onResponse(Call<RegisterResponse> call, Response<RegisterResponse> response) {
+                if (!response.isSuccessful()){
+                    Toast.makeText(RegisterActivityFifthStep.this, "Code "+response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(RegisterActivityFifthStep.this, "Successfully registered", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<RegisterResponse> call, Throwable t) {
+                Toast.makeText(RegisterActivityFifthStep.this, "Error "+t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void binding() {
         imgBtnBackFifthStep = findViewById(R.id.imgBtnBackFifthStep);
         imgBtnUpload = findViewById(R.id.imgBtnUpload);
         btnSignUpFinal = findViewById(R.id.btnSignUpFinal);
--- app/src/main/java/com/study/twitter/url/Url.java
+++ app/src/main/java/com/study/twitter/url/Url.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Url {
-    public static final String base_url = "http://10.0.2.2:3000/";
+    public static final String base_url = "http://10.0.2.2:3012/";
     public static String token = "Bearer ";
     public static String imagePath = base_url + "uploads/" ;
 
